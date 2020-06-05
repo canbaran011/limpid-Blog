6f4cb9644c438aaa87376efabf62d0f744215d0d
Can Baran Gündüz <canbaran011@gmail.com>
2020-06-05 12:37:59 +0300

    templates of boot added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,75 @@
 <template>
     <div>
-        <div class="container-md bordered mt-3">
-            <h3>
-            welcome to the land of limpid.
-        </h3>
-        <p> 
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque saepe, nemo eius numquam natus consectetur veniam repellendus illo voluptate, voluptas voluptates quia cupiditate quod accusamus deleniti quo, tempore non obcaecati libero consequuntur molestias. Illum dolorem provident rerum omnis. Nisi quo eveniet facilis dicta hic amet laudantium. Voluptas quam maxime modi?        </p>
+        <!-- Page Content -->
+  <div class="container">
+
+    <!-- Jumbotron Header -->
+    <header class="jumbotron my-4">
+      <h1 class="display-3">A welcome from _limpid!</h1>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+      <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
+    </header>
+
+    <!-- Page Features -->
+    <div class="row text-center">
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
         </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
     </div>
 </template>
 <script>
--- src/components/blogs/BlogAdd.vue
+++ src/components/blogs/BlogAdd.vue
@@ -1,8 +1,24 @@
 <template>
-    <div>
-        <div class="container-sm">
-     this is blog ad page   
-
+  <div>
+    <div class="container-md">
+        <h3 style="margin-top:15px"><b>Add a new _limpid blog</b> </h3>
+      <form>
+        <div class="form-group">
+          <label for="exampleFormControlInput1">limpid Title</label>
+          <input class="form-control form-control-lg" type="text" placeholder="a limpid and pure title">
         </div>
+        
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">limpid Content</label>
+          <textarea
+            placeholder="..."
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+          <button type="submit" class="btn btn-primary mb-2">Submit</button>
+      </form>
     </div>
+  </div>
 </template>
